fec8ab0e278e3a5a0935d4f0d72fc431d608c120
Antony Cabeza <antonycabeza@gmail.com>
2021-03-14 18:52:26 -0400

    Haciendo el navbar responsive y añadiendo imgs
    Se optimizo el navbar para dispositivos moviles, se modifico el tamaño
    del hero para pantallas mas pequeñas y se añadieron imagenes a la
    seccion de investigacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const burger = document.querySelector('.header--burger');
+const cancel = document.querySelector('.header--cancel');
+const navbar = document.querySelector('.navbar');
+
+// Abrir el navbar
+burger.addEventListener('click', () => {
+    navbar.classList.add('navbar-inside');
+    burger.style.display = 'none'
+    cancel.style.display = 'initial'
+})
+// Cerrar el navbar
+cancel.addEventListener('click', () => {
+    navbar.classList.remove('navbar-inside');
+    cancel.style.display = 'none'
+    burger.style.display = 'initial'
+})
+
+// Navbar fixed con scroll
+
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
